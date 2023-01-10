bfc0de8d768bd9c54eb7fbdc4764dbb5f8436551
marcosmarquesde <marquesdiv@hotmail.com>
2023-01-10 10:32:59 -0300

    nova home finalizada

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
 //trabalhando no login
 
 //finalizando sistema de login
+
+function login(nome, email){
+    return "bem vindo";
+}
+
+//Sistema de login completo
+//Retornamos o nome do usuario logado
